eea258c36d8f693b6323e2a8ec0bc685c2ddddd6
Eric Fialkowski <eric.fialkowski@gmail.com>
2019-03-02 14:25:00 -0700

    adding precise interval and cond to prevent multiple runs

--- watch.go
+++ watch.go
@@ -9,12 +9,15 @@ import (
 	"time"
 
 	"github.com/buger/goterm"
+	"github.com/tevino/abool"
 )
 
 var interval = flag.Int("n", 5, "Interval in seconds")
 var runWithCommand = flag.Bool("x", false, "Run with cmd.exe")
 var hideTitle = flag.Bool("t", false, "Hide title bar")
 var exitOnError = flag.Bool("e", false, "Exit on non-zero return of command")
+var preciseInterval = flag.Bool("p", false, "Try to run at precise interval")
+var cond = abool.New()
 
 func init() {
 	flag.Parse()
@@ -42,10 +45,19 @@ func main() {
 	}
 
 	run(time.Now(), cmd, cmdArgs)
-	ticker := time.NewTicker(time.Duration(*interval) * time.Second)
+	nextRun := time.Now().Add(time.Duration(*interval) * time.Second)
+	ticker := time.NewTicker(time.Duration(10 * time.Millisecond))
 	go func() {
 		for t := range ticker.C {
-			run(t, cmd, cmdArgs)
+			if time.Now().After(nextRun) || time.Now().Equal(nextRun) {
+				startOfRun := time.Now()
+				run(t, cmd, cmdArgs)
+				if *preciseInterval {
+					nextRun = startOfRun.Add(time.Duration(*interval) * time.Second)
+				} else {
+					nextRun = time.Now().Add(time.Duration(*interval) * time.Second)
+				}
+			}
 		}
 	}()
 
@@ -53,34 +65,37 @@ func main() {
 }
 
 func run(t time.Time, name string, args []string) {
-	goterm.Clear()
-	goterm.MoveCursor(1, 1)
-	if !*hideTitle {
-		goterm.Printf("Every %ds: %s %s", *interval, name, strings.Join(args, " "))
-		width := goterm.Width()
-		ts := t.Format("Mon Jan _2 15:04:05 2006")
-		hn, err := os.Hostname()
-		if err == nil {
-			s := fmt.Sprintf("%s: %s", hn, ts)
-			goterm.MoveCursor(width-len(s), 1)
-			goterm.Print(s)
-		} else {
-			goterm.MoveCursor(width-len(ts), 1)
-			goterm.Print(ts)
-		}
+	if cond.SetToIf(false, true) {
+		goterm.Clear()
+		goterm.MoveCursor(1, 1)
+		if !*hideTitle {
+			goterm.Printf("Every %ds: %s %s", *interval, name, strings.Join(args, " "))
+			width := goterm.Width()
+			ts := t.Format("Mon Jan _2 15:04:05 2006")
+			hn, err := os.Hostname()
+			if err == nil {
+				s := fmt.Sprintf("%s: %s", hn, ts)
+				goterm.MoveCursor(width-len(s), 1)
+				goterm.Print(s)
+			} else {
+				goterm.MoveCursor(width-len(ts), 1)
+				goterm.Print(ts)
+			}
 
-		goterm.MoveCursor(2, 3)
-	}
-	cmd := exec.Command(name, args...)
-	output, err := cmd.CombinedOutput()
-	if err == nil {
-		goterm.Print(string(output))
-	} else {
-		goterm.Printf("Error: %v", err)
-		if *exitOnError {
-			goterm.Flush()
-			os.Exit(1)
+			goterm.MoveCursor(2, 3)
 		}
+		cmd := exec.Command(name, args...)
+		output, err := cmd.CombinedOutput()
+		if err == nil {
+			goterm.Print(string(output))
+		} else {
+			goterm.Printf("Error: %v", err)
+			if *exitOnError {
+				goterm.Flush()
+				os.Exit(1)
+			}
+		}
+		goterm.Flush()
+		cond.UnSet()
 	}
-	goterm.Flush()
 }
